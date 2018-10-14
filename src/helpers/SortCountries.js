c59551953776637e7465e11dcbcdc6fdfb7586c4
khadeejadin <khadeejadin@gmail.com>
2018-10-14 00:24:03 -0400

    Complete sorting functionality

--- src/helpers/SortCountries.js
+++ src/helpers/SortCountries.js
@@ -3,24 +3,27 @@ export function SortCountries(countries, sortType) {
     let sortedCountries = countries;
     switch (sortType) {
         case 'total':
-            sortedCountries = countries
-            .sort((a, b) => a.total - b.total);
+            sortedCountries.sort((a, b) => compare(a, b, 'total', 'gold'));
             break;
         case 'gold':
-            sortedCountries = countries
-            .sort((a, b) => a.gold - b.gold);
+            sortedCountries.sort((a, b) => compare(a, b, 'gold', 'silver'));
             break;
         case 'silver':
-            sortedCountries = countries
-            .sort((a, b) => a.silver - b.silver);
+            sortedCountries.sort((a, b) => compare(a, b, 'silver', 'gold'));
             break;
         case 'bronze':
-            sortedCountries = countries
-            .sort((a, b) => a.bronze - b.bronze);
+            sortedCountries.sort((a, b) => compare(a, b, 'bronze', 'gold'));
             break;
         default:
           console.log('Default');
     }
-
     return sortedCountries;
+}
+
+function compare(a, b, first, second) {
+    if (a[`${first}`] === b[`${first}`])
+    {       
+        return a[`${second}`] > b[`${second}`] ? -1 : 1;
+    }
+    return b[`${first}`] - a[`${first}`];
 }
